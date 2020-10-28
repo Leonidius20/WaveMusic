00634c7253d21bfc3c712d0e73067e53fccf57d3
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-10-28 21:00:28 +0200

    made slide-out side menu (no animation yet) and improved main menu on mobile

--- interface.js
+++ interface.js
@@ -1,16 +1,39 @@
 let navbar;
+let sideMenu;
 let navbarHidden = true;
+let sideMenuHidden = true;
 
 window.onload = () => {
     navbar = document.getElementById('navbar');
+    sideMenu = document.getElementById('side-menu-items');
 
     document.getElementById('hamburger-button').onclick = () => {
         if (navbarHidden) {
             navbar.style.display = 'flex';
+            navbar.style.opacity = '1';
         } else {
             navbar.style.display = 'none';
+            navbar.style.opacity = '0';
         }
         navbarHidden = !navbarHidden;
+    }
+
+    document.getElementById('side-menu-button').onclick = () => {
+        console.log(sideMenuHidden);
+        if (sideMenuHidden) {
+            sideMenu.style.display = 'block';
+            sideMenu.style.width = '50vw';
+        } else {
+            sideMenu.style.display = 'none';
+            sideMenu.style.width = '0';
+        }
+        sideMenuHidden = !sideMenuHidden;
+    }
+
+    document.getElementById('close-side-menu-button').onclick = () => {
+        sideMenu.style.display = 'none';
+        sideMenu.style.width = '0';
+        sideMenuHidden = true;
     }
 }
 
