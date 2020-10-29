71d423e0e8f3f949bf6c09c67018dd269ddfc2f0
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-10-29 22:25:52 +0200

    styled buttons and input

--- interface.js
+++ interface.js
@@ -1,3 +1,5 @@
+const MOBILE_WIDTH = 560;
+
 let navbar;
 let sideMenu;
 let navbarHidden = true;
@@ -21,25 +23,23 @@ window.onload = () => {
     document.getElementById('side-menu-button').onclick = () => {
         console.log(sideMenuHidden);
         if (sideMenuHidden) {
-            sideMenu.style.display = 'block';
+            //sideMenu.style.display = 'block';
             sideMenu.style.width = '50vw';
         } else {
-            sideMenu.style.display = 'none';
+            //sideMenu.style.display = 'none';
             sideMenu.style.width = '0';
         }
         sideMenuHidden = !sideMenuHidden;
     }
 
     document.getElementById('close-side-menu-button').onclick = () => {
-        sideMenu.style.display = 'none';
-        sideMenu.style.width = '0';
-        sideMenuHidden = true;
+        closeSideMenu();
     }
 }
 
 // because js applies style inline in html and it has to be cleared
 window.onresize = () => {
-    if (window.innerWidth > 560) { // not a mobile device anymore
+    if (window.innerWidth > MOBILE_WIDTH) { // not a mobile device anymore
         navbar.style.display = 'flex';
         navbar.style.opacity = '1';
         for (const property of sideMenu.style) {
@@ -50,3 +50,18 @@ window.onresize = () => {
     }
 }
 
+function closeSideMenu() {
+    sideMenu.style.width = '0';
+    sideMenuHidden = true;
+}
+
+function onSideMenuItemSelected() {
+    closeSideMenu();
+}
+
+function closeNavBar() {
+    if (window.innerWidth <= MOBILE_WIDTH) { // on mobile devices
+        navbar.style.display = 'none';
+        navbarHidden = true;
+    }
+}
